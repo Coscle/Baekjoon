6eae06c12f754631a57c331fb0162f6fd5212122
Coscle <newsrain@naver.com>
2022-09-13 23:28:47 +0900

    class1

---
+++ class1/Baekjoon_9498.py
@@ -0,0 +1,12 @@
+N = int(input())
+N = N // 10
+if N == 10 or N == 9:
+    print("A")
+elif N == 8:
+    print("B")
+elif N == 7:
+    print("C")
+elif N == 6:
+    print("D")
+else:
+    print("F")
